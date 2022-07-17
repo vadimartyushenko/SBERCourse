7009ad448e701a312574470a7fcc4ed8af3e09af
Vadim Artyushenko <artushenkomail@mail.ru>
2022-07-18 00:09:21 +0700

    update

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import tests as test
 import functions as lib
+from datetime import *
 
 if __name__ == '__main__':
     
+    print(datetime.today())
     for i in test.cases:
         if lib.count_all(i[1], i[2]) == i[3]:
             print("Test #" + str(i[0]) + ': OK!')
